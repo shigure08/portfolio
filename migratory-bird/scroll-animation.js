1af66795ef5b81208fd71264e32bed084bcd7dc1
時雨 <66808223+shigure08@users.noreply.github.com>
2021-06-03 09:32:20 +0900

    Add files via upload

---
+++ migratory-bird/scroll-animation.js
@@ -0,0 +1,38 @@
+$(function(){
+  const obj = $(".scroll-animation-obj");
+  const hopIn = $(".scroll-animation-hop");
+  const leftIn = $(".scroll-animation-left");
+  const rightIn = $(".scroll-animation-right");
+  $(window).on('scroll',function(){
+    obj.each(function(){
+      const objPos = $(this).offset().top;
+      const scroll =$(window).scrollTop();
+      const windowH = $(window).height();
+      if(scroll > objPos - windowH){
+        $(this).css({
+          'opacity':'1'
+        });
+      }else{
+        $(this).css({
+          'opacity':'0'
+        });
+      }
+    });
+    hopIn.each(function(){
+      const objPos = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowH = $(window).height();
+      if(scroll > objPos - windowH){
+        $(this).css({
+          'transform':'translate(0,0)'
+          /*'transform':'scale(1,1)'*/
+        });
+      }else{
+        $(this).css({
+          'transform':'translate(0,60px)'
+          /*'transform':'scale(0,0)'*/
+        });
+      }
+    });
+  });
+});
